26ffc5a61c5acc6f459a240a4562b6b469c961e1
nata13pr <117272943+Nata13pr@users.noreply.github.com>
2024-04-13 21:52:52 +0000

    feat:added map and data

--- src/Skill.js
+++ src/Skill.js
@@ -1,7 +1,14 @@
 import "./styles.css";
 
-export default function Skill (props){
-    return (
-        <div className="skill" style={props.color}>{props.skill}{props.image}</div>
-    )
+export default function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: `${props.skill.color}` }}>
+      <span>{props.skill.skill}</span>
+      <span>
+        {props.skill.level === "advanced" && "😅"}
+        {props.skill.level === "intermediate" && "😝"}
+        {props.skill.level === "beginner" && "😇"}
+      </span>
+    </div>
+  );
 }
--- src/SkillList.js
+++ src/SkillList.js
@@ -1,14 +1,13 @@
-import Skill from './Skill.js';
+import Skill from "./Skill.js";
+import skills from "./data.js";
 import "./styles.css";
 
-export default function SkillList(props){
-    return(
-        <div className='skill-list'>
-           <Skill  skill='Javascript' image='😅' color={{backgroundColor: 'red'}}/>
-           <Skill  skill='Typescript' image='😇' color={{backgroundColor: 'blue'}}/>
-           <Skill  skill='React' image='😘' color={{backgroundColor: 'purple'}}/>
-           <Skill  skill='Node.js' image='😝' color={{backgroundColor: 'orange'}}/>
-           <Skill  skill='Html' image='🤨' color={{backgroundColor: 'yellow'}}/>
-        </div>
-    )
+export default function SkillList(props) {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill} key={skill.skill} />
+      ))}
+    </div>
+  );
 }
